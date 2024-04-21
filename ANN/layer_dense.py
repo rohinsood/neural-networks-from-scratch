3d77e30dfd906c9902acd8a99f9ce83f507a597d
rohinsood <rs.rohinsood@gmail.com>
2024-04-21 12:59:07 -0700

    create layer structure

---
+++ ANN/layer_dense.py
@@ -0,0 +1,46 @@
+import numpy as np 
+
+class Layer:
+    def __init__(self, inputs, neurons, weight_regularizer_l1=0, weight_regularizer_l2=0, bias_regularizer_l1=0, bias_regularizer_l2=0):
+        self.weights = 0.01 * np.random.randn(inputs, neurons)
+        self.biases = np.zeros((1, neurons))
+        #set regularization strength
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        self.bias_regularizer_l2 = bias_regularizer_l2
+
+
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+        
+    def backward(self, dvalues):
+        #Gradient calculation on the parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradients on regularization
+        # L1 on weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+            
+        # L2 on weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+
+        # L1 on biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+
+        # L2 on biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
+        #Gradient Calculation on values
+        self.dinputs  = np.dot(dvalues, self.weights.T)
+        
---
+++ ANN/layer_input.py
@@ -0,0 +1,4 @@
+class Layer_Input():
+    #forward pass
+    def forward(self, inputs):
+        self.output = inputs
